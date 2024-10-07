74e6e605c2adedc9c91a365207f8bd6b3a045b6d
Gunjan Jain <jain10gunjan@gmail.com>
2024-10-07 21:04:51 +0530

    Fix

--- admin/src/components/userService.js
+++ admin/src/components/userService.js
@@ -1,11 +1,7 @@
 // src/services/userService.js
-import axios from "axio";
+import axios from "axios";
 
-<<<<<<< HEAD
 const API_URL = "https://general-work.vercel.app/api/users/getAllUsers"; // Adjust to your API base URL
-=======
-const API_URL = 'https://general-work.vercel.app/api/users/getAllUsers'; // Adjust to your API base URL
->>>>>>> 85835430fa647925356e3d856801e2d8d1031149
 
 export const fetchAllUsers = async () => {
   try {
